e8c39f2dcd2623d76f0f3c1747b0d12f0fceb88a
Lavínia Mariano <84093830+ninamariano@users.noreply.github.com>
2021-09-01 09:17:45 -0300

    Add files via upload

--- tamanhotitulo.js
+++ tamanhotitulo.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", inserirBotoesComInnerHTML);
+
+function inserirBotoesComInnerHTML(){
+  div = document.createElement("div");
+  div.style.backgroundColor = "black";
+  div.innerHTML = `
+  <button onclick="tamanho('x-large')">-</button>
+  <button onclick="tamanho('xxx-large')">+</button>
+  `;
+  document.body.prepend(div);
+}
+
 function tamanho(size){
     h1 = document.querySelector('h1');
     h1.style.fontSize = size;
